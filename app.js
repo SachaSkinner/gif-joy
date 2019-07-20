d40453e95f74aa982c5edc87f03ada77ce35aa1d
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-07-20 15:20:52 -0500

    added 2 states - still and animate with 2 links, added a rating in span

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var array = ["cat", "dog", "horse"];
+var array = ["penguin", "monkey", "cat", "lion", "dog", "horse"];
+var click = 0;
 
 LoadButtons();
 
-$(document).on("click", ".animal-btn", function(){
+$(document).on("click", ".animal-btn", function () {
     var pet = $(this).val();
-    GiphySearch(pet)
+    giphySearch(pet)
 })
 
- // This .on("click") function will trigger the AJAX Call
- $("#find-animal").on("click", function (event) {
+
+$(document).on("click", ".image", function () {
+    console.log("You clilcked me")
+
+
+
+})
+
+// This .on("click") function will trigger the AJAX Call
+$("#find-animal").on("click", function (event) {
 
     // event.preventDefault() can be used to prevent an event's default behavior.
     // Here, it prevents the submit button from trying to submit a form when clicked
@@ -18,28 +27,28 @@ $(document).on("click", ".animal-btn", function(){
     var animal = $("#animal-input").val();
 
     array.push(animal)
-    GiphySearch(animal);
+    giphySearch(animal);
     LoadButtons();
 });
 
-function LoadButtons(){
+function LoadButtons() {
     $("#buttons").html("")
-    
+
     for (var i = 0; i < array.length; i++) {
         var button = $("<button>");
         button.val(array[i]);
         button.text(array[i]);
-        button.addClass("animal-btn")
+        button.addClass("animal-btn");
 
 
         $("#buttons").append(button).append(" ");
-    }  
+    }
 }
- 
-function GiphySearch(value){
+
+function giphySearch(value) {
     $("#images").html("")
     // Here we construct our URL
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ value +"&limit=10&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + value + "&limit=10&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
 
     $.ajax({
         url: queryURL,
@@ -47,30 +56,52 @@ function GiphySearch(value){
     })
         // After the data from the AJAX request comes back
         .then(function (response) {
-            console.log(response);
             // Saving the image_original_url property
+            console.log(response);
 
-            for(var i = 0; i < response.data.length; i++){
+            for (var i = 0; i < response.data.length; i++) {
 
-                var imageUrl = response.data[i].images.fixed_height_small_still.url;
-    
-                // Creating and storing an image tag
-                var catImage = $("<img>");
-    
-                // Setting the catImage src attribute to imageUrl
-                catImage.attr("src", imageUrl);
-                //catImage.attr("alt", "cat image");
-    
-                // Prepending the catImage to the images div
-                $("#images").prepend(catImage);
+                var stillUrl = response.data[i].images.fixed_height_still.url;
+                var animateUrl = response.data[i].images.fixed_height.url;
+                var ratingUrl = response.data[i].rating;
+
+                var div = $("<div>");
+
+                var rating = $("<span>");
+                rating.text("Rating: " + ratingUrl);                
+                
+                var image = $("<img>");
+                // Setting the src attribute to stillUrl
+                image.attr("src", stillUrl);
+                image.attr("state", "still");
+                image.attr("src-still", stillUrl);
+                image.attr("src-animate", animateUrl);
+                image.addClass("image");
+
+                div.append(image);
+                div.append(rating);
+
+                $("#images").append(div);  
+
+
             }
+
+            $(".image").on("click", function () {
+                var state = $(this).attr("state");
+                
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("src-animate"));                        
+                    $(this).attr("state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("src-still"));
+                    $(this).attr("state", "still");
+                }
+
+            });
+
         });
-    // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
-    // and display it in the div with an id of movie-view
 
-    // ------YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE.
-
-    //
 }
- 
- 
+
+
+
